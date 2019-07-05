9136c7a62a1314be651db1e42ecf649f11d50130
Mary Selig <seligmar@gmail.com>
2019-07-05 10:15:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -295,12 +295,12 @@ def winning_team #find the team with the most points
         away_team_points << nested_hash[:points] 
       end
     end
-    home_team_points_total = home_team_points.reduce(:+)
-  end 
+  #  home_team_points_total = home_team_points.reduce(:+)
+#  end 
     away_team_points_total = home_team_points.reduce(:+)
-  end
-  end 
-end
+#   end
+#   end 
+# end
 #home_team_points
 end
 
